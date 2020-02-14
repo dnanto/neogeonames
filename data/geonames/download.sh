f4ce2fd039d113805eed77b34089c923e75f6ec6
dnanto <dnegron2@gmu.edu>
2020-02-13 20:23:13 -0500

    create prototype Rmd file and geonames data repo with a sample data file to normalize...

---
+++ data/geonames/download.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+curl -O http://download.geonames.org/export/dump/readme.txt
+curl -O http://download.geonames.org/export/dump/countryInfo.txt
+curl -O http://download.geonames.org/export/dump/cities500.zip
+exit
+
