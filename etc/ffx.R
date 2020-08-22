da0d00e7ae410be60dfea67cd7ceb873f71650d1
dnanto <dnegron2@gmu.edu>
2020-08-22 00:47:23 -0400

    update logo

--- etc/ffx.R
+++ etc/ffx.R
@@ -1,12 +1,12 @@
+library(ggplot2)
 ggsave(
   file.path("man", "figures", "ffx.svg"),
-  filter(map_data("county"), region == "virginia", subregion == "fairfax") %>%
-    ggplot() +
-    geom_polygon(aes(long, lat), fill = "#3F562A") +
-    theme_minimal() +
-    theme(
-      axis.title = element_blank(),
-      axis.text = element_blank(),
-      panel.grid = element_blank()
-    )
+  ggplot(dplyr::filter(map_data("county"), region == "virginia", subregion == "fairfax")) +
+  geom_polygon(aes(long, lat), fill = "#A53A2B") +
+  theme_minimal() +
+  theme(
+    axis.title = element_blank(),
+    axis.text = element_blank(),
+    panel.grid = element_blank()
+  )
 )
